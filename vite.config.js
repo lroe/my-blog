d25add386edeff7e9520cd11a0c0421620152698
Jeevan Kumar <jeevanms1337@gmail.com>
2026-07-29 16:21:31 +0530

    Fix Vite dev vs production index.html conflict

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
   plugins: [react()],
   base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   server: {
     port: 5173,
     proxy: {
